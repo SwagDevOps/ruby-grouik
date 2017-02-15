ce850dd419f44d64dd39fb2a37a7d590489b313d
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-02-15 08:31:14 +0100

    Grouik::Process#on_errors(&block) added
    permit to exit (during a rake task), sample of use:
    
    ~~~~
    task 'src/ceres.rb' do
      require 'grouik'
    
      Grouik.process do |grouik|
        grouik.verbose  = false
        grouik.paths    = ['lib']
        grouik.basedir  = 'src'
        grouik.output   = 'src/ceres.rb'
        grouik.template = 'src/ceres.tpl'
      end.on_errors { exit Errno::ECANCELED::Errno }
    end
    ~~~~

--- src/lib/grouik/process.rb
+++ src/lib/grouik/process.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
+
 require 'pathname'
 
-# Intended to wrap Grouik::Loader and easify reusability through Grouik::Cli
+# Intended to wrap ``Grouik::Loader``
+#
+# Easify reusability through ``Grouik::Cli``
 # or as standalone (for example in a Rake task)
 class Grouik::Process
   attr_accessor :template
@@ -23,6 +26,7 @@ class Grouik::Process
     !!(@verbose)
   end
 
+  # @return [Pathname]
   def output=(output)
     @output = output.is_a?(String) ? Pathname.new(output) : output
   end
@@ -49,8 +53,13 @@ class Grouik::Process
     loader.format(options)
   end
 
+  # @return [Hash]
   def errors
     loader.errors
+  end
+
+  def has_errors?
+    !errors.empty?
   end
 
   def display_errors
@@ -64,6 +73,7 @@ class Grouik::Process
     self
   end
 
+  # @return [self]
   def display_status
     message  = '%s: %s files; %s iterations; %s errors (%.4f) [%s]'
     statuses = {true  => :success, false => :failure}
@@ -97,6 +107,12 @@ class Grouik::Process
     loader.loaded?
   end
 
+  def on_errors(&block)
+    block.call(self) if has_errors?
+
+    self
+  end
+
   # Provides access to public accessors
   def method_missing(method, *args, &block)
     unless respond_to_missing?(method)
@@ -113,16 +129,19 @@ class Grouik::Process
 
   protected
 
+  # @return [Grouik::Loader]
   def loader
     @loader
   end
 
+  # @return [Array]
   def loader_attributes
     loader.public_methods
       .grep(/^\w+=$/)
       .map {|m| m.to_s.gsub(/=$/, '').to_sym }
   end
 
+  # @return [Array]
   def loader_accessors
     loader_attributes + loader_attributes.map { |m| ('%s=' % m).to_sym }
   end
