a4c608c3f8f8ed2371c9637ab63b9f816c33f085
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-03-21 08:33:17 +0100

    Grouik::Cli uses Grouik::Helpers::Cli

--- src/lib/grouik/cli.rb
+++ src/lib/grouik/cli.rb
@@ -6,6 +6,7 @@ require 'pathname'
 require 'yaml'
 
 require 'grouik' unless defined?(Grouik)
+require 'grouik/concerns'
 
 # Grouik command line interface
 #
@@ -14,6 +15,8 @@ class Grouik::Cli
   attr_reader :argv
   attr_reader :options
   attr_reader :arguments
+
+  include Grouik::Concerns::Helpable
 
   class << self
     # Program name
@@ -34,7 +37,6 @@ class Grouik::Cli
     def defaults
       {
         stats: true,
-        verbose: true,
         paths: ['.'],
         basedir: '.',
         output: STDOUT,
@@ -64,27 +66,8 @@ class Grouik::Cli
   #
   # @return [OptionParser]
   def parser
-    options = @options
-    parser = OptionParser.new do |opts|
-      opts.banner = 'Usage: %s [options] [FILE]' % self.program_name
-      opts.on('--basedir=BASEDIR', 'Basedir [%s]' % options[:basedir]) \
-      { |v| options[:basedir] = v }
-      opts.on('-o=OUTPUT', '--output=OUTPUT', 'Output [/dev/stdout]') do |v|
-        options[:output] = v
-      end
-      opts.on('-r=REQUIRE', '--require=REQUIRE', 'Required file on startup') do |v|
-        options[:require] = v
-      end
-
-      opts.on('--ignores x,y,z', Array, 'Ignores') \
-      { |v| options[:ignores] = v }
-      opts.on('--paths x,y,z', Array, 'Paths') \
-      { |v| options[:paths] = v }
-      opts.on('--[no-]stats', 'Display some stats') \
-      { |v| options[:stats] = v }
-      opts.on('-v', '--[no-]verbose', 'Run verbosely') \
-      { |v| options[:verbose] = v }
-    end
+    parser = helpers.get(:cli).make_parser(@options)
+    parser.banner = 'Usage: %s [options] [FILE]' % self.program_name
 
     parser
   end
@@ -120,10 +103,12 @@ class Grouik::Cli
       )
     else
       arguments.each do |filepath|
+        config = helpers.get(:cli).read_config(filepath)
+
         processables << OpenStruct.new(
           path: Pathname.new(filepath).dirname,
           file: Pathname.new(filepath),
-          options: self.options.merge(config_from_path(filepath)),
+          options: config,
           'file?' => true
         )
       end
@@ -144,22 +129,6 @@ class Grouik::Cli
     0
   end
 
-  # Read a config file
-  #
-  # @param [String] path
-  # @return [Hash]
-  def config_from_path(path)
-    file = Pathname.new(path.to_s)
-
-    if file.exist? and file.file?
-      h = YAML.safe_load(file.read).each_with_object({}) { |(k, v), h| h[k.intern] = v; }
-      h.each do |k, v|
-      end
-      return h
-    end
-    {}
-  end
-
   protected
 
   # Initiate and run a new ``Process`` from options
@@ -167,7 +136,7 @@ class Grouik::Cli
   # @param [Hash] options
   # @return [Grouik::Process]
   def process(options)
-    options = prepare_options(options)
+    options = helpers.get(:cli).prepare_options(options)
 
     process = Grouik.process do |process|
       process.basedir   = options.fetch(:basedir)
@@ -176,7 +145,6 @@ class Grouik::Cli
       process.output    = options.fetch(:output)
       process.template  = options[:template]
       process.bootstrap = options[:require]
-      process.verbose   = !!(options[:verbose])
     end
 
     process.on_success do |process|
@@ -187,34 +155,5 @@ class Grouik::Cli
     end
 
     process
-  end
-
-  # Prepare options
-  #
-  # Process values in order to easify their use
-  #
-  # @param [Hash] options
-  # @return [Hash]
-  def prepare_options(options)
-    [:require, :output].each do |k|
-      next unless options[k]
-      begin
-        options[k] = Pathname.new(options[k])
-      rescue TypeError
-        next
-      end
-      unless options[k].absolute?
-        options[k] = Pathname.new(Dir.pwd).join(options[k])
-      end
-    end
-
-    [:ignores, :paths].each do |k|
-      next unless options[k]
-      options[k] = [options[k]] if options[k].is_a? String
-
-      options[k] = options[k].to_a.map { |s| /#{s}/ } if :ignores == k
-    end
-
-    options
   end
 end
