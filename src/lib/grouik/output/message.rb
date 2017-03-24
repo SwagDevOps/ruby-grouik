8a647d534b35f281b4b0a1805e666cb602798324
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-03-24 08:59:34 +0100

    license applied to sources (in src)

--- src/lib/grouik/output/message.rb
+++ src/lib/grouik/output/message.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Copyright (C) 2017 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 # Describe a message (sent on a IO as STDOUT/STDERR)
 class Grouik::Output::Message
   attr_accessor :content
