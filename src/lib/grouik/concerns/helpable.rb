ad69e4ace862ed5e55d2d78e5152613b649642a9
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-03-30 08:46:35 +0200

    missing (optional) require statement

--- src/lib/grouik/concerns/helpable.rb
+++ src/lib/grouik/concerns/helpable.rb
@@ -6,6 +6,8 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
+require 'active_support/concern'
+
 # Provide ``helpers`` method
 module Grouik::Concerns::Helpable
   extend ActiveSupport::Concern
